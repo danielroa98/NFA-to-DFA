aa86910830dc160b3903dc413d782f5f62e935f3
danielroa98 <daniel.roa98@gmail.com>
2019-10-24 15:44:56 -0500

    No se ha modificado nada

--- NFA-DFA.py
+++ NFA-DFA.py
@@ -1,6 +1,9 @@
 #   Second Partial Project
 #
 #   Daniel Roa          -   A01021960
-#   Sebastian Vives     -   A01 
-#   Sergio Hernandez    -   A01
-#   Christian Dalma     -   A01
+#   Sebastian Vives     -   A01
+#   Sergio Hernandez    -   A01069420
+#   Christian Dalma     -   A01423166
+import numpy
+
+file = input("Inserte el nombre del .txt\n")
