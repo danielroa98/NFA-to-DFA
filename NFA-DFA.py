fc646305b4a1e3fdbd3679edf41223baa400db14
danielroa98 <daniel.roa98@gmail.com>
2019-10-24 16:02:55 -0500

    NP

--- NFA-DFA.py
+++ NFA-DFA.py
@@ -4,6 +4,14 @@
 #   Sebastian Vives     -   A01
 #   Sergio Hernandez    -   A01069420
 #   Christian Dalma     -   A01423166
-import numpy
+import numpy as np
 
-file = input("Inserte el nombre del .txt\n")
+#file = open("NFA_test_file_Aug_Dec_2019.txt", "r")
+
+#if file.mode == 'r':
+#    contenido = file.read()
+#    print(contenido)
+
+nfaINPUT = np.loadtxt(fname = "/mnt/c/Users/danie/Programacion/5t0 Semestre/NFA-to-DFA/NFA_test_file_Aug_Dec_2019.txt")
+
+print(nfaINPUT)
