f06dbb2a8461f6ff9eab88526bad7d43fdc9edfd
danielroa98 <daniel.roa98@gmail.com>
2019-10-24 17:21:03 -0500

    se añadió función de Numpy

--- NFA-DFA.py
+++ NFA-DFA.py
@@ -4,6 +4,7 @@
 #   Sebastian Vives     -   A01
 #   Sergio Hernandez    -   A01069420
 #   Christian Dalma     -   A01423166
+
 #file = open("NFA_test_file_Aug_Dec_2019.txt", "r")
 
 #if file.mode == 'r':            # r = leer documento
@@ -13,9 +14,10 @@ import numpy as np
 
 def readFile(file):
 	with open(file, 'r') as f:
-		a = [[x for x in line.replace('{', '').replace('}', '').replace(
-			'(', '').replace(')', '').replace('\n', "").split(',')] for line in f]
+		a = [[x for x in line.replace('{', '').replace('}', '').replace('(', '').replace(')', '').replace('\n', "").split(',')] for line in f]
 	a = np.array(a)
 	array2D = a.reshape((a.size/3, 3))
 	return array2D
 
+for x in range(len(array2D)):
+    print array2D[x],
