fc075b33a21b1be972aa06b67c4532448dc8f776
Daniel Roa <daniel.roa98@gmail.com>
2019-10-25 00:05:32 -0500

    Ya imprime en un documento predefinido

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import itertools
 
-myfile = open("NFA.txt","r")
+myfile = open("NFA_test_file_Aug_Dec_2019.txt","r")
 counter = 0
 lista = []
 Qn = []
 
+outFile = open("OutputDemo15.txt", "w+")
+
+outFile.write("Original input: \n")
+
 if myfile.mode == 'r':
     c = myfile.readline()
+    outFile.write(c)
     print(c)
+
+outFile.close()
+
+outFile = open("OutputDemo15.txt", "a")
+outFile.write("\nQn:\n")
+outFile.close()
+
+outFile = open("OutputDemo15.txt", "a")
 
 for x in c:
     if x != '{' and x != '(' and x != ','and x != ')'and x != '}' and x != '\n':
@@ -17,7 +30,19 @@ for x in c:
 
 print(lista)
 print("Qn: ", Qn)
+listToStr = ' '.join(map(str, Qn))
 
+outFile.write(listToStr)
+
+outFile.close()
+
+outFile = open("OutputDemo15.txt", "a")
+outFile.write("\n")
+outFile.close()
+
+outFile = open("OutputDemo15.txt", "a")
+outFile.write("\nQd:\n{")
+outFile.close()
 QD = []
 # Hacer QD donde seran 2^4 posibilidades
 for L in range(0, len(Qn)+1):
@@ -25,6 +50,17 @@ for L in range(0, len(Qn)+1):
         QD.append(subset)
 
 print("QD: ",QD)
+listToStr = ' '.join(map(str, QD)) 
+
+outFile = open("OutputDemo15.txt", "a")
+
+outFile.write(listToStr)
+outFile.write("}")
+outFile.close()
+
+outFile = open("OutputDemo15.txt", "a")
+outFile.write("\n\nFd:\n{")
+outFile.close()
 
 #Obtener FD (Final state)
 F = Qn[-1] #Obtenemos el ultimo elemento en la lista Qn
@@ -32,5 +68,16 @@ FD = []
 for x in QD:
     if F in x:
         FD.append(x)
-    
+
 print("FD: ",FD)
+listToStr = ' '.join(map(str, FD)) 
+
+outFile = open("OutputDemo15.txt", "a")
+
+outFile.write(listToStr)
+outFile.write("}")
+outFile.close()
+
+outFile = open("OutputDemo15.txt", "a")
+outFile.write("\n")
+outFile.close()
