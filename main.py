fd6bc9068ff44372e1dd238bb5e06739d7d1d0e7
danielroa98 <daniel.roa98@gmail.com>
2019-11-22 07:34:36 -0600

    Updated values

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#   Second Partial Project
+#
+#   Daniel Roa          -   A01021960
+#   Sebastian Vives     -   A01
+#   Sergio Hernandez    -   A01069420
+#   Christian Dalma     -   A01423166
 import itertools
 
 myfile = open("NFA_test_file_Aug_Dec_2019.txt","r")
@@ -15,6 +21,10 @@ if myfile.mode == 'r':
     print(c)
 
 outFile.close()
+
+#outFile.open("OutputDemo15.txt", "a")
+#outFile.write("\nSigma:\n")
+
 
 outFile = open("OutputDemo15.txt", "a")
 outFile.write("\nQn:\n")
@@ -81,3 +91,10 @@ outFile.close()
 outFile = open("OutputDemo15.txt", "a")
 outFile.write("\n")
 outFile.close()
+
+print("\nDelta: ")
+
+print("\t\t\t0\t1")
+for elem in QD:
+    print("_______________________")
+    print(elem, " -|")
